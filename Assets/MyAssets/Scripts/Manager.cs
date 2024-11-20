64c62a840c0af46043e79bd63fe887c468adc6a4
Yiğit Top <yigit.toq@gmail.com>
2024-11-21 01:13:45 +0300

    Node  Collision Settings

--- Assets/MyAssets/Scripts/Manager.cs
+++ Assets/MyAssets/Scripts/Manager.cs
@@ -1,5 +1,9 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Manager : MonoBehaviour
 {
+    [SerializeField] private List<GameObject[]> _objects = new();
+
+    public static int nodeCount = 0;
 }
--- Assets/MyAssets/Scripts/System/CameraFollow.cs
+++ Assets/MyAssets/Scripts/System/CameraFollow.cs
@@ -1,14 +1,17 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public float smoothTime;
-
-    public Transform target;
-
-    public Vector3 offset;
-
     private Vector3 velocity = Vector3.zero;
+
+    [SerializeField] private Vector3 offset;
+
+    [SerializeField] private Transform target;
+
+    [SerializeField] private float smoothTime;
+
+    private bool isFinish;
 
     private void LateUpdate()
     {
@@ -17,6 +20,25 @@ public class CameraFollow : MonoBehaviour
             Vector3 targetPosition = target.position + offset;
 
             transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+        }
+    }
+
+    private void Update()
+    {
+        if (!isFinish)
+        {
+            Physics.Raycast(target.position, target.forward, out RaycastHit hit, 5, LayerMask.GetMask("Finish"));
+            if (hit.collider != null)
+            {
+                Debug.DrawLine(target.position, hit.point, Color.red);
+
+                if (hit.collider.CompareTag("Finish"))
+                {
+                    Camera.main.fieldOfView = 50;
+
+                    isFinish = true;
+                }
+            }
         }
     }
 }
